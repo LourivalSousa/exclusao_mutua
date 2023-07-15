668a876b8c328b4d9e23a230a2379c50f79c6ef8
Lourival Sousa <lourivalsousadev@gmail.com>
2023-07-14 23:02:33 -0300

    feature: coordenador de processos e criador de processor adicionado

---
+++ process_coordinator.py
@@ -0,0 +1,86 @@
+import socket
+import sys
+import threading
+
+IP = '192.168.0.117'
+PORT = 5000
+CONECTION_LIMIT = 10
+process_queue = []  
+process = {}  
+lock = threading.Lock()
+
+def grant_acess():
+    while True:
+        if process_queue:
+            pid = process_queue[0]
+            send_message(pid, "GRANT")
+            print("Processo ", pid, "na regiao critica.\n")
+            process_queue.pop(0)
+
+def handle_conections():
+  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server.bind((IP, PORT))
+  server.listen(CONECTION_LIMIT)
+
+  while True:
+      client, client_addr = server.accept()
+      thread_client = threading.Thread(target=handle_process, args=(client,))
+      thread_client.start()
+  
+def handle_process(self, client):
+  pid = client.recv(1024).decode()
+  process[pid] = client
+  lock.acquire()
+  process_queue.append(pid)
+  lock.release()
+  print("Processo", pid, "conectado ao coordenador!\n")
+
+
+def send_message(pid, message):
+    if pid in process:
+        socket_cliente = process[pid]
+        socket_cliente.send(message.encode())
+
+def show_queue():
+    print("\nProcesso na fila: \n")
+   
+    lock.acquire()
+    if process_queue:
+        for pid in process_queue:
+            print("Processo de id: ", pid,"\n")
+    else:
+        print("Nenhum processo na fila.\n")
+    lock.release()
+
+def conected_process():
+    print("\nProcessos conectados: \n")
+    lock.acquire()
+    if process:
+        for pid, client in process.items():
+            print(f"Processo {pid} - Atendido", client, "\n")
+    else:
+        print("Nenhum processo conectado\n")
+    lock.release()
+
+def start_coordinator():
+    thread_conect = threading.Thread(target=handle_conections)
+    thread_conect.start()
+    thread_execute = threading.Thread(target=grant_acess)
+    thread_execute.start()
+
+    while True:
+        option = input("\nOpcoes: \n1-> Mostrar lista de processos \n2-> Mostrar fila \n3-> Sair\n")
+        
+        if option == "1":
+            conected_process()  
+        elif option == "2":
+            show_queue() 
+        elif option == "3":
+            print('Saindo...')
+            sys.exit(0)
+        else:
+            print('Opcao invalida.\n')
+
+if __name__ == "__main__":
+     start_coordinator()
+
---
+++ process_requester.py
@@ -0,0 +1,61 @@
+import threading
+import socket
+import time
+
+def iniciar(process,loops, k):
+    for _ in range(loops):
+        solicitar_secao_critica(process)
+        entrar_secao_critica(process)
+
+        time.sleep(k)
+        sair_secao_critica(process)
+
+def solicitar_secao_critica(process):
+    mensagem = f"1|{process.pid}|".ljust(process.size, '0')
+    enviar_mensagem(process,mensagem)
+
+def enviar_mensagem(process, mensagem):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((process.ip_coordenador, process.porta_coordenador))
+    client.sendall(mensagem.encode())
+    client.close()
+
+def entrar_secao_critica(process):
+    with open("resultado.txt", "a") as arquivo:
+        hora_atual = time.strftime("%Y-%m-%d %H:%M:%S.%f")
+        arquivo.write(f"Processo {process.id_processo} entrou na seção crítica no horario: {hora_atual}\n")
+
+def sair_secao_critica(process):
+    with open("resultado.txt", "a") as arquivo:
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S.%f")
+        arquivo.write(f"Processo {process.id_processo} saiu da seção crítica no horario {current_time}\n")
+
+if __name__ == "__main__":
+    quant_process = int(input("Insira a quantidade de processos: \n"))
+    k = int(input("Insira o valor de k, o formato deve ser em segundos.: \n"))
+    repet = int(input("Insira a quantidade de repeticoes: \n"))
+    size = int(input("Insira em bytes o tamanho das mensagens: \n"))
+
+    process_array = []
+
+    for i in range(quant_process):
+        pid = str(i + 1)
+        
+        process = {}
+
+        process["id_processo"] = pid
+        process["ip_coordenador"] = "192.168.0.117"
+        process["porta_coordenador"] = 5000
+        process["tamanho_mensagem"] = size
+
+        process_array.append(process)
+
+    threads = []
+
+    for process in process_array:
+        thread = threading.Thread(target=iniciar, args=(process, repet, k))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
