66383ce443f1e3e4f91195340bfec28e3127af5c
Lourival Sousa <lourivalsousadev@gmail.com>
2023-07-14 23:23:11 -0300

    ajuste

--- process_requester.py
+++ process_requester.py
@@ -12,24 +12,24 @@ def iniciar(process,loops, k):
         sair_secao_critica(process)
 
 def solicitar_secao_critica(process):
-    mensagem = f"1|{process.pid}|".ljust(process.size, '0')
+    mensagem = f"1-{process['pid']}|".ljust(process['size'], '0')
     enviar_mensagem(process,mensagem)
 
 def enviar_mensagem(process, mensagem):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((process.ip, process.port))
+    client.connect((process["ip"], process['port']))
     client.sendall(mensagem.encode())
     client.close()
 
 def entrar_secao_critica(process):
     with open("resultado.txt", "a") as arquivo:
         hora_atual = time.strftime("%Y-%m-%d %H:%M:%S.%f")
-        arquivo.write(f"Processo {process.pid} entrou na seção crítica no horario: {hora_atual}\n")
+        arquivo.write(f"Processo {process['pid']} entrou na seção crítica no horario: {hora_atual}\n")
 
 def sair_secao_critica(process):
     with open("resultado.txt", "a") as arquivo:
         current_time = time.strftime("%Y-%m-%d %H:%M:%S.%f")
-        arquivo.write(f"Processo {process.pid} saiu da seção crítica no horario {current_time}\n")
+        arquivo.write(f"Processo {process['pid']} saiu da seção crítica no horario {current_time}\n")
 
 if __name__ == "__main__":
     quant_process = int(input("Insira a quantidade de processos: \n"))
